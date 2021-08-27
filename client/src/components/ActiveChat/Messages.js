2ab3b930acc2309db35902c0f7f3504be1346b40
haristanvir <haristanvir@gmail.com>
2021-08-27 09:14:48 -0400

    removes unneeded import

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, useRadioGroup } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
